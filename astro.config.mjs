c6ebc1071b31216adbc474ebbf4216ebc630a5fc
gabriel barros <gabrielbarrosba@gmail.com>
2022-12-01 13:35:55 -0300

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gabrielbarrosba.github.io',
-    base: '/plataforma_publisocial'
+    site: 'https://gabrielbarrosba.github.io'
 });
